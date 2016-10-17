8f451e2968e489a0e042a41a656aa13c5a46460d
Richard Young <info@hyperlathe.com>
2016-10-17 17:42:07 +0100

    Update. Nothing significant.

--- inc/header.php
+++ inc/header.php
@@ -1,19 +1,11 @@
-<nav class="uk-navbar"  data-uk-sticky="{media: 640}">
-    <a class="uk-navbar-brand uk-hidden-small" href="#"  data-uk-smooth-scroll><img src="img/logo.png" alt="HyperLathe"></a>
-    <ul class="uk-navbar-nav uk-hidden-small uk-navbar-flip">
-        <li class="uk-active">
-          <a href="layouts_frontpage.html">Technologies</a>
-        </li>
-        <li>
-          <a href="layouts_portfolio.html">Projects</a>
-        </li>
-        <li>
-          <a href="layouts_blog.html">About</a>
-        </li>
-        <li>
-          <a href="layouts_contact.html">Contact</a>
-        </li>
-    </ul>
-    <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
-    <a class="uk-navbar-brand uk-navbar-center uk-visible-small" href="#"  data-uk-smooth-scroll><img src="img/logo.png" alt="HyperLathe"></a>
-</nav>
+<div class="sidebar">
+<ul class="navigation">
+  <li class="nav-item"><a href="#">Home</a></li>
+  <li class="nav-item"><a href="#">Design</a></li>
+  <li class="nav-item"><a href="#">Development</a></li>
+  <li class="nav-item"><a href="#">Other</a></li>
+  <li class="nav-item"><a href="#">Contact</a></li>
+</ul>
+</div>
+<input type="checkbox" id="nav-trigger" class="nav-trigger" />
+<label for="nav-trigger"></label>
--- index.php
+++ index.php
@@ -10,9 +10,18 @@
 
 
 <body>
-
-  
 <?php include("./inc/header.php"); ?>
+<div class="site-wrap">
+  <h1>HyperLathe.com</h1>
+  <h3>Home </h3>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vero nisi eos sed qui natus, ut eius reprehenderit error nesciunt veniam aliquam nulla itaque labore obcaecati molestiae eveniet, perferendis provident amet perspiciatis expedita accusantium! Eveniet, quos voluptas et, labore natus, saepe unde est nulla sit eaque tempore debitis accusantium. Recusandae.</p>
+  <p>Dolorem aliquam a libero reiciendis obcaecati doloribus ipsa eos laudantium, dicta in! Odit iure ut ratione, dolorum cupiditate perferendis voluptatum sapiente, dignissimos sunt necessitatibus, reprehenderit consequatur dolorem. Aliquam veniam quaerat, pariatur deserunt reiciendis vero vitae, repellat omnis sequi dolor nesciunt. Nihil similique alias impedit, obcaecati eligendi delectus voluptatum! Ipsum, vel.</p>
+  <p>Sint, perspiciatis nemo aut, rerum excepturi deleniti modi quos nihil corporis eum, maiores soluta labore, consectetur eligendi nesciunt. Placeat, incidunt! Illum placeat eligendi, veritatis consectetur eum! Dolor obcaecati minima ab placeat voluptatem neque modi doloribus, magnam qui voluptate eaque in. Nulla expedita hic porro architecto facere officiis vitae numquam, dolor!</p>
+  <p>Perferendis quis ea incidunt ducimus nisi voluptate natus. Repellat asperiores quod rerum rem quos blanditiis enim modi, veniam voluptas a facilis! Velit cum omnis, voluptatum eum inventore! Corrupti, suscipit, neque distinctio expedita est laboriosam cum aliquid minus tempora quaerat officia possimus unde vel deleniti eaque fugit accusamus iusto dolorum natus.</p>
+  
+</div>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
